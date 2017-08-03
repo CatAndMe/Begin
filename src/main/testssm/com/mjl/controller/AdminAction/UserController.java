77c273b2e12880b32a89227087c4682f04a063ad
IceWater <735342600@qq.com>
2017-08-03 17:51:30 +0800

    2017.8.3

--- src/main/testssm/com/mjl/controller/AdminAction/UserController.java
+++ src/main/testssm/com/mjl/controller/AdminAction/UserController.java
@@ -7,9 +7,13 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 //@Controller注解用于标示本类为web层控制组件
 @Controller
@@ -23,35 +27,50 @@ public class UserController {
         UserService userService;
 
         //login业务的访问位置为/user/login
-    @RequestMapping("/login")
-//    @ResponseBody
-       public String login(User user,HttpServletRequest request,Model model){
-//        List<User> userList=userService.getAllUser();
-//        model.addAttribute("userList",userList);
+       @RequestMapping("/login")
+       @ResponseBody
+       public Object login(HttpServletRequest request,@RequestParam Integer loginId,@RequestParam String password){
+        boolean message;
         HttpSession it=request.getSession();
-        System.out.println(it);
-
+        Map<String,Object> messageJeson=new HashMap<String, Object>();
         //调用login方法来验证是否是注册用户
-        boolean loginType = userService.login(user.getId(),user.getPassword());
+        boolean loginType = userService.login(loginId,password);
         if(loginType){
+            User user=userService.selectById(loginId);
             //如果验证通过,则将用户信息传到前台
             User user1=userService.selectById(user.getId());
             user1.setPassword(null);
             request.setAttribute("user",user1);
             it.setAttribute("user",user1);
-
-            //并跳转到success.jsp页面
-            return "view/adminView";
+            message=true;
+            messageJeson.put("message",message);
+            return messageJeson;
         }else{
             //若不对,则将错误信息显示到错误页面
-            request.setAttribute("message","用户名密码错误");
-            return "UserView/LoginError";
+            message=false;
+            messageJeson.put("message",message);
+            return messageJeson;
         }
-    }
+       }
 
-    @RequestMapping("/hallo")
-       public String hallo(){
-           return "UserView/login";
+    @RequestMapping("/modifyPassword")
+    @ResponseBody
+       public Object hallo(HttpServletRequest request,@RequestParam String fistInput,@RequestParam String secondInput){
+        boolean message;
+        HttpSession it=request.getSession();
+        User user=(User)request.getSession().getAttribute("user");
+        Map<String,Object> messageJeson=new HashMap<String, Object>();
+        if(fistInput.toString().equals(secondInput.toString())!=false){
+            user.setPassword(secondInput);
+            userService.updateUserById(user);
+            message=true;
+            return messageJeson.put("message",message);
+        }else {
+            message=false;
+            messageJeson.put("message",message);
+            return messageJeson;
+        }
+
     }
 
     @RequestMapping("/logout")

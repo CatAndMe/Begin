0833894dd32af2affeb6cbe5810b3f8adf27c642
IceWater <735342600@qq.com>
2017-08-14 20:00:37 +0800

    2017.08.14

---
+++ src/main/webapp/Strass/jss/emplInfoJs.js
@@ -0,0 +1,373 @@
+
+
+var pageSize = "8";//每页行数
+var currentPage = "1";//当前页
+var totalPage = "4";//总页数
+var rowCount = "4";//总条数
+var params="";//参数
+var emplName_="";
+var email_="";
+var fistInput_="";
+var secondInput_="";
+var emplId_=0;
+function changeInfo(emplId_Info) {
+    var params={fistInput:fistInput_,secondInput:secondInput_,emplId:emplId_Info};
+    $.post("/user/modifyPassword.do",params,
+        function(data){
+            if (data.message==true){
+                alert("(づ￣3￣)づ╭❤～成功");
+                hideDiv('pop-div');
+            }else {
+                alert("(づ￣3￣)づ╭❤～失败");
+            }
+
+        })
+
+}
+function changeEmplEmail(emplId_) {
+    var params={emplId:emplId_,emplEmail:email_};
+    $.post("/user/modifyEmail.do",params,
+        function(data1){
+            if (data1.message==true){
+                alert("(づ￣3￣)づ╭❤～成功");
+                hideDiv('pop-div');
+            }else {
+                alert("(づ￣3￣)づ╭❤～失败");
+            }
+
+        })
+}
+function add_user() {
+    var content;
+    content=
+        "<br>\n" +
+        "                    <h3 style=\"text-align:center;font-size:20px\">信息修改</h3>\n" +
+        "                    <h4 style=\"text-align:center;border-top:3px solid #286a3a\"></h4>\n" +
+        "                    <br>\n" +
+        "                    <br>\n" +
+        "                    <div style=\"text-align:center\">\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">输入名字：</th>\n" +
+        "                            <td><input id=\"addInfo_Name\" type=\"text\" style=\"width: 130px\"/></td>\n" +
+        "\n" +
+        "                        </tr>\n" +
+        "                        <br>\n" +
+        "                        <br>\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">输入密码：</th>\n" +
+        "                            <td><input id=\"changeInfo1\" type=\"text\" style=\"width: 130px\"/></td>\n" +
+        "\n" +
+        "                        </tr>\n" +
+        "                        <br>\n" +
+        "                        <br>\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">再次输入：</th>\n" +
+        "                            <td><input id=\"changeInfo2\" type=\"text\" style=\"width: 130px\"/></td>\n" +
+        "\n" +
+        "                        </tr>\n" +
+        "                        <br>\n" +
+        "                        <br>\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">输入工号：</th>\n" +
+        "                            <td><input id=\"addInfo_Id\" type=\"text\" style=\"width: 130px\"/></td>\n" +
+        "\n" +
+        "                        </tr>\n" +
+        "                        <br>\n" +
+        "                        <br>\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">输入邮箱：</th>\n" +
+        "                            <td><input id=\"addInfo_Email\" type=\"text\" style=\"width: 130px\"/></td>\n" +
+        "\n" +
+        "                        </tr>\n" +
+        "                        <br>\n" +
+        "                        <br>\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">权限设置：</th>\n" +
+        "                           <select name=\"search-sort\" id=\"addInfo_Admin\" style=\"width: 60x\">\n" +
+        "                                    <option value=\"0\">员工</option>\n" +
+        "                                    <option value=\"1\">管理员</option>\n" +
+        "                           </select>"+"　　　　　"+
+    "                        </tr>\n" +
+    "                        <br>\n" +
+    "                        <br>\n" +
+    "                        <div id='buttonPanel'  style=\"text-align:center\">\n" +
+    "\n" +
+    "                            <input style=\"margin:0 20px\" type=\"button\" value=\"取消\" id=\"btn1\" class=\"btn btn-primary btn2\" onclick=\"hideDiv('pop-div') ;\" />\n" +
+    "                            <input style=\"margin:0 20px\" type=\"button\" value=\"确认\" id=\"btn2\" class=\"btn btn-primary btn2\" onclick=\"checkSuer();\" />\n" +
+    "                        </div>\n" +
+    "                        <br>";
+    popupDiv(400,450,content);
+}
+function update_user(id) {
+    var content;
+    content=
+        "<br>\n" +
+        "                    <h3 style=\"text-align:center;font-size:20px\">信息修改</h3>\n" +
+        "                    <h4 style=\"text-align:center;border-top:3px solid #286a3a\"></h4>\n" +
+        "                    <br>\n" +
+        "                    <div style=\"text-align:center\">\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">输入密码：</th>\n" +
+        "                            <td><input id=\"changeInfo1\" type=\"text\" style=\"width: 90px\"/></td>\n" +
+        "\n" +
+        "                        </tr>\n" +
+        "                        <br>\n" +
+        "                        <br>\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">再次输入：</th>\n" +
+        "                            <td><input id=\"changeInfo2\" type=\"text\" style=\"width: 90px\"/></td>\n" +
+        "\n" +
+        "                        </tr>\n" +
+        "                        <br>\n" +
+        "                        <br>\n" +
+        "                        <tr>\n" +
+        "                            <th style=\"font-size: 16px\">邮箱修改：</th>\n" +
+        "                            <td><input value='"+id+"' id=\"emplId_update\" type=\"hidden\"><input id=\"changeInfo3\" type=\"text\" style=\"width: 90px\"/></td>\n" +
+        "                        </tr>\n" +
+        "                        <br>\n" +
+        "                        <br>\n" +
+        "                        <div id='buttonPanel'  style=\"text-align:center\">\n" +
+        "\n" +
+        "                            <input style=\"margin:0 20px\" type=\"button\" value=\"取消\" id=\"btn1\" class=\"btn btn-primary btn2\" onclick=\"hideDiv('pop-div') ;\" />\n" +
+        "                            <input style=\"margin:0 20px\" type=\"button\" value=\"确认\" id=\"btn2\" class=\"btn btn-primary btn2\" onclick=\"checkSuer();\" />\n" +
+        "                        </div>\n" +
+        "                        <br>";
+    popupDiv(250,250,content);
+}
+function queryForPages(){
+    var params={pageNum:currentPage,pageSize:pageSize,emplName_:emplName_,emplId_:emplId_};
+    $.post("/Action/userList.do",params,
+        function(data){
+//                    var results = JSON.parse(data);
+            var admin="";
+            var results = data;
+            var list = results.Data;
+            totalPage = results.totalPageCount;
+            rowCount = results.totalItemCount;
+            pageNo=results.pageNum;
+            var message=results.Message;
+            var s = '';
+            if (message!=null){
+                alert(message);
+            }
+
+            if(list.length != 0){
+                for(var i=0; i<list.length; i++){
+                    if (list[i].emplAdmin>0){
+                        admin="管理员";
+                    }else {
+                        admin="用户";
+                    }
+                    s += '<tr><td>' +(i+1)
+                        + '</td><td>' + list[i].username
+                        + '</td><td>' + list[i].id
+                        + '</td><td>' + list[i].email
+                        + '</td><td>' + admin
+                        + '</td><td><a href="#" onclick="update_user(\''+list[i].id+'\')">修改</a>'
+                        + '</td></tr>';
+
+                }
+
+                $('#tb').html(s);
+                $(emplId).val(null);
+            } else{
+                $('#tb').html("");
+                $(emplId).val(null);
+            }
+        })
+
+}
+function popupDiv(width,height,content) {
+    // 获取传入的DIV
+    var $div_obj = $("#pop-div");
+//            style="width: 250px;height:250px;"
+    $div_obj.height(height);
+    $div_obj.width(width);
+    // 计算机屏幕高度
+    var windowWidth = $(document.body).width();
+    // 计算机屏幕长度
+    var windowHeight = $(document.body).height();
+    // 取得传入DIV的高度
+//            var popupHeight = $div_obj.height();
+    // 取得传入DIV的长度
+//            var popupWidth = $div_obj.width();
+
+    // // 添加并显示遮罩层
+    $("<div id='bg'></div>").width(windowWidth)
+        .height(windowHeight).click(function() {
+        //hideDiv(div_id);
+    }).appendTo("body").fadeIn(200);
+    $div_obj.html(
+        content
+    )
+    $div_obj.css({
+        "position" : "absloute",
+        "left" : windowWidth / 2 - height / 2.3,
+        "top" : windowHeight / 2.4 - width /2.3,
+
+        "opacity" : "show"
+    }).animate({
+
+        opacity : "show"
+    }, "slow");
+
+
+}
+function hideDiv() {
+    $("#bg").remove();
+    $("#pop-div").animate({
+//                left : 0,
+//                top : 0,
+        opacity : "hide"
+
+    }, "slow");
+
+}
+function checkAdd_user() {
+
+    if(checkName($("#addInfo_Name").val())==true){
+        if (checkPassword()==true){
+            if (changeEmplEmail()==true){
+                if(checkId($("#addInfo_Id").val())){
+                    if ($("#addInfo_Admin").val()){
+
+                    }else {
+
+                    }
+                }else {
+
+                }
+            }else {
+
+            }
+        }else {
+
+        }
+    }else {
+
+    }
+}
+function checkSuer() {
+    fistInput_=$("#changeInfo1").val();
+    secondInput_=$("#changeInfo2").val();
+    email_=$("#changeInfo3").val();
+    var emplId_Info=$("#emplId_update").val();
+    if (fistInput_ && secondInput_ && !email_){
+        if (checkPassword(emplId_Info)==true){
+            changeInfo(emplId_Info);
+        }
+    }else if(fistInput_ && secondInput_ && email_){
+
+        if (checkEmail(email_)==true){
+            changeEmplEmail(emplId_Info);
+            if (checkPassword(emplId_Info)==true){
+                changeInfo(emplId_Info);
+            }
+        }else {
+            alert("(＝。＝)邮箱格式有误");
+        }
+    }else if(!fistInput_ && !secondInput_ && email_){
+        if (checkEmail(email_)==true){
+            changeEmplEmail(emplId_Info);
+
+        }else {
+            alert("(＝。＝)邮箱格式有误");
+        }
+
+    }else {
+        if (!secondInput_ || !fistInput_){
+            alert("请输入密码");
+        }else {
+            hideDiv('pop-div');
+        }
+
+    }
+}
+function checkEmail(str){
+    var re = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/;
+    if (re.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function checkId(str){
+    var re =/^[0-9]{1,20}$/;
+    if (re.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function checkName(str){
+    var re =/^[a-zA-Z]{1,30}$/;
+    if (re.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function checkPassword(emplId_Info) {
+    fistInput_=$("#changeInfo1").val();
+    secondInput_=$("#changeInfo2").val();
+    var PASSWORD_FORMAT =/^[A-Za-z0-9]+$/;
+    if(fistInput_ && secondInput_==null){
+        alert("(＝。＝)密码不能为空");
+    }else if(secondInput_.length<6||fistInput_.length<6){
+        alert("(＝。＝)密码不能小于六位哦");
+    }else if(fistInput_!=secondInput_){
+        alert("╮(╯_╰)╭两次密码输入不一致");
+    }else if(PASSWORD_FORMAT.test(fistInput_)==false||PASSWORD_FORMAT.test(secondInput_)==false){
+        alert("╮(╯_╰)╭密码组成只能是数字或字母");
+    }else {
+        return true
+    }
+}
+$(document).ready(
+
+    function(){
+
+        $('#basic-modal .basic').click(function () {
+            $('#basic-modal-content').modal();
+            return false;
+        });
+
+        //员工号查询查询
+        $("#emplIdBotton").click(function() {
+            emplId_ = $(emplId).val();
+            if(EMOLID_FORMAT.test(emplId_)==false && emplId_!=null){
+                alert("(＞﹏＜)您输入的员工号格式有误,请输入数字");
+            }else {
+                queryForPages();
+            }
+
+        });
+        //首页
+        $("#firstPage").click(function(){
+            currentPage="1";
+            queryForPages();
+        });
+        //上一页
+        $("#previous").click(function(){
+            if(currentPage>1)
+            {
+                currentPage-- ;
+            }
+            queryForPages();
+        });
+        //下一页
+        $("#next").click(function(){
+            if(currentPage<totalPage)
+            {
+                currentPage++ ;
+            }
+            queryForPages();
+        });
+        //尾页
+        $("#last").click(function(){
+            currentPage = totalPage;
+            queryForPages();
+        });
+        queryForPages();
+    }
+);
